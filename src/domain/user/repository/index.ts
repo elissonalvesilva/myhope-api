a058f9dedec647d37a08cfdac7c0ffb78b9c2e81
Elisson Alves Silva <nossilesilva@gmail.com>
2023-03-14 19:11:08 -0300

    feat: created user repositoru

--- src/domain/user/repository/index.ts
+++ src/domain/user/repository/index.ts
@@ -3,8 +3,9 @@ import User from "@/domain/user/entity";
 export default interface UserRepository {
   getUserById(id: string): Promise<User | Error>
   getUserByEmail(email: string): Promise<User | Error>
-  createUser(user: User): Promise<boolean | Error>
-  updateUser(user: User): Promise<boolean | Error>
   getResetCodeByUserId(id: string): Promise<number | Error>
+  createUser<T>(user: User): Promise<T>
+  updateUser(user: User): Promise<boolean | Error>
   updatePassword(id: string, password: string): Promise<boolean | Error>
 }
+
