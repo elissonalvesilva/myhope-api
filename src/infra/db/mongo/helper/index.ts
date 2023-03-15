81fa927ffa32f928ab7ffc3e32e5351692e0cc32
Elisson Alves Silva <nossilesilva@gmail.com>
2023-03-15 15:15:37 -0300

    feat: added mongoose

---
+++ src/infra/db/mongo/helper/index.ts
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+export class MongoHelper {
+  static connect(uri: string): Promise<void> {
+    const options = {
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    };
+
+    const connection = () => {
+      mongoose
+        .connect(encodeURI(uri), options)
+        .then(() => {
+          return console.log(`Successfully connected to ${uri}`);
+        })
+        .catch((error: Error) => {
+          console.log('Error connecting to database: ', error);
+          return process.exit(1);
+        });
+    };
+    return new Promise((resolve) => resolve(connection()));
+  }
+
+  static disconnect(): void {
+    mongoose.connection.close();
+  }
+}
