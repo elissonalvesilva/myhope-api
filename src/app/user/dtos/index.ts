b927fbc7eef1126ed151aee0b98d688ff3e63a1d
Elisson Silva <nossilesilva@gmail.com>
2023-03-22 18:41:08 -0300

    feat: added dto

--- src/app/user/dtos/index.ts
+++ src/app/user/dtos/index.ts
@@ -2,6 +2,20 @@ import Account from "@/domain/account/entity";
 import Quiz from "@/domain/quiz/entity";
 import { Status } from "@/domain/user/entity";
 
+export interface UserCreatedResponseDTO {
+  name: string;
+  lastName: string;
+  email: string;
+  image?: string;
+  account: {
+    id: string,
+    accountNumber: string,
+    balance: number
+  };
+  status: Status;
+}
+
+
 export default interface UserDTO {
   name: string;
   lastName: string;
