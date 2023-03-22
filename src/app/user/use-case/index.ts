649eb6216fc1c28fe326d333a0f8d33de8f5a097
Elisson Silva <nossilesilva@gmail.com>
2023-03-22 18:44:18 -0300

    refactor: change user app

--- src/app/user/use-case/index.ts
+++ src/app/user/use-case/index.ts
@@ -1,9 +1,12 @@
+import { Result } from "true-myth";
 import AccountFactory from "@/domain/account/factory";
 import { AccountRepository } from "@/domain/account/repository";
 import UserRepository from "@/domain/user/repository";
 import Hashing from "@/app/protocols/hashing";
-import User from "@/domain/user/entity";
+import User, { UserResponse } from "@/domain/user/entity";
 import AccountService from "@/domain/account/services/account-number";
+import UserError from "@/app/user/error";
+import { UserCreatedResponseDTO } from "../dtos";
 
 
 export default class UserApplication {
@@ -13,42 +16,79 @@ export default class UserApplication {
     private readonly hashingId: Hashing,
   ){}
 
-  async getUserById(id: string): Promise<User> {
-    try {
-      return await this.userRepository.getUserById(id);
-    } catch (error) {
-      throw error
+  async getUserById(id: string): Promise<Result<UserResponse, UserError>> {
+    const response = await this.userRepository.getUserById(id);
+    if(!response) {
+      return Result.err(new UserError({
+        name: "ERR_USER_NOT_FOUND",
+        message: "user not found"
+      }));
     }
+    return Result.ok(response);
   }
 
-  async getUserByEmail(email: string): Promise<User> {
-    try {
-      return await this.userRepository.getUserByEmail(email);
-    } catch (error) {
-      throw error;
+  async getUserByEmail(email: string): Promise<Result<UserResponse, UserError>> {
+    const response = await this.userRepository.getUserByEmail(email);
+    if(!response) {
+      return Result.err(new UserError({
+        name: "ERR_USER_NOT_FOUND",
+        message: "user not found",
+        cause: { email }
+      }));
     }
+    return Result.ok(response);
   }
 
-  async getResetCode(id: string): Promise<number | Error> {
-    return await this.userRepository.getResetCodeByUserId(id);
+  async getResetCode(id: string): Promise<Result<number, UserError>> {
+    const code = await this.userRepository.getResetCodeByUserId(id);
+    if(!code) {
+      return Result.err(new UserError({
+        name: "ERR_RESET_CODE_USER_NOT_FOUND",
+        message: "reset code not found for this user",
+      }))
+    }
+    return Result.ok(code);
   }
 
-  async createUser(user: User): Promise<boolean> {
-    try {
-      const userCreated = await this.userRepository.createUser(user);
-      const accountNumber = AccountService.accountNumber();
-      const accountId = this.hashingId.hash();
-      const buildedAccount = AccountFactory.create(
-        accountId,
+  async createUser(user: User): Promise<Result<UserCreatedResponseDTO, UserError>> {
+    const userCreated = await this.userRepository.createUser(user);
+    if(!userCreated) {
+      return Result.err(new UserError({
+        name: "ERR_USER_NOT_CREATED",
+        message: "user cant be created",
+      }));
+    }
+
+    const accountNumber = AccountService.accountNumber();
+    const accountId = this.hashingId.hash();
+    const buildedAccount = AccountFactory.create(
+      accountId,
+      accountNumber,
+      0,
+    );
+
+    const account = await this.accountRepository.createAccount(buildedAccount, userCreated.id);
+    if(!account) {
+      return Result.err(new UserError({
+        name: "ERR_CREATE_ACCOUNT",
+        message: "error to create account"
+      }));
+    }
+
+    const userResponse: UserCreatedResponseDTO = {
+      name: user.name,
+      lastName: user.lastName,
+      email: user.email,
+      image: user.image,
+      account: {
+        id: accountId,
         accountNumber,
-        0,
-      );
+        balance: 0,
+      },
+      status: user.status,
+    } 
 
-      await this.accountRepository.createAccount(buildedAccount, userCreated.id);
-      return true;
-    } catch (error) {
-      throw error
-    }
+    return Result.ok(userResponse);
   }
 
 }
