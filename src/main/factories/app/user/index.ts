8e8eb80f5dac8d788c4f991d020bda91fd564f88
Elisson Silva <nossilesilva@gmail.com>
2023-04-09 14:11:28 -0300

    fix: fix impl

--- src/main/factories/app/user/index.ts
+++ src/main/factories/app/user/index.ts
@@ -3,11 +3,13 @@ import { makeJWT } from "@/main/factories/infra/jwt";
 import { makeUserImplementation } from "@/main/factories/infra/db/mongodb/user";
 import { makeAccountImpl } from "@/main/factories/infra/db/mongodb/account";
 import { makeCrypto } from "@/main/factories/infra/crypto";
+import { makeQuizImpl } from "@/main/factories/infra/db/mongodb/quiz";
 
 export const makeUserApp = (): UserApplication => {
   const userRepositoryImpl = makeUserImplementation();
   const accountRepositoryImpl = makeAccountImpl();
   const jwtImpl = makeJWT();
   const encrypt = makeCrypto()
-  return new UserApplication(userRepositoryImpl, accountRepositoryImpl, jwtImpl, encrypt);
+  const quizImpl = makeQuizImpl();
+  return new UserApplication(userRepositoryImpl, accountRepositoryImpl, jwtImpl, encrypt, quizImpl);
 };
