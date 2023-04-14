48f0f3c49ffcece7dd7b4cf6751e48c57d71bcda
Elisson Silva <nossilesilva@gmail.com>
2023-04-14 19:31:49 -0300

    refactor: change headers

--- src/main/middlewares/cors.ts
+++ src/main/middlewares/cors.ts
@@ -1,8 +1,13 @@
 import { Request, Response, NextFunction } from 'express';
 
 export const cors = (req: Request, res: Response, next: NextFunction): void => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', '*');
-  res.setHeader('Access-Control-Allow-Methods', '*');
+  const allowedOrigins = ['http://localhost:3000', 'https://myhope.vercel.app'];
+  const origin = req.headers.origin || 'http://localhost:3000';
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Referer");
+  res.header("Access-Control-Allow-credentials", 'true');
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE, OPTIONS");
   next();
 };
